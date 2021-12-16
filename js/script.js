8cfcf1b906292f220c34ebbbe3bcb64b3d48093b
Kirill Makhnev <74978665+notequal21@users.noreply.github.com>
2021-12-16 17:04:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,51 @@
 
-const element = document.querySelector('.js-choice');
-const element2 = document.querySelector('.js-choice2');
-const element3 = document.querySelector('.js-choice3');
-const choices = new Choices(element, {
-    searchEnabled: false,
-})
-const choices2 = new Choices(element2, {
-    searchEnabled: false,
-})
-const choices3 = new Choices(element3, {
-    searchEnabled: false,
-})
 
-const burgerBtnOpen = document.querySelector('.header-body__col-burger')
-const burgerBtnClose = document.querySelector('.mobile-menu__top-close')
-const mobileMenu = document.querySelector('.mobile-menu')
-const body = document.querySelector('body')
+if (document.querySelector('.js-choice')) {
+    const element = document.querySelector('.js-choice');
+    const choices = new Choices(element, {
+        searchEnabled: false,
+    })
+}
+if (document.querySelector('.js-choice2')) {
+    const element = document.querySelector('.js-choice2');
+    const choices2 = new Choices(element, {
+        searchEnabled: false,
+    })
+}
+if (document.querySelector('.js-choice3')) {
+    const element = document.querySelector('.js-choice3');
+    const choices3 = new Choices(element, {
+        searchEnabled: false,
+    })
+}
 
-if (burgerBtnOpen) {
+// mobile selects 
+if (document.querySelector('.js-choice-mob')) {
+    const element = document.querySelector('.js-choice-mob');
+    const choices = new Choices(element, {
+        searchEnabled: false,
+    })
+}
+if (document.querySelector('.js-choice2-mob')) {
+    const element = document.querySelector('.js-choice2-mob');
+    const choices2 = new Choices(element, {
+        searchEnabled: false,
+    })
+}
+if (document.querySelector('.js-choice3-mob')) {
+    const element = document.querySelector('.js-choice3-mob');
+    const choices3 = new Choices(element, {
+        searchEnabled: false,
+    })
+}
+
+if (document.querySelector('.header-body__col-burger')) {
+    
+    const burgerBtnOpen = document.querySelector('.header-body__col-burger')
+    const burgerBtnClose = document.querySelector('.mobile-menu__top-close')
+    const mobileMenu = document.querySelector('.mobile-menu')
+    const body = document.querySelector('body')
+
     burgerBtnOpen.addEventListener('click', () => {
         if (mobileMenu.classList.contains('active')) {
             mobileMenu.classList.remove('active')
@@ -40,11 +68,13 @@ if (burgerBtnOpen) {
 
 
 
-const filterBtnOpen = document.querySelector('.mobile-filter__btn')
-const filterBtnClose = document.querySelector('.mobile-filter__close')
-const mobileFilter = document.querySelector('.mobile-filter__body')
+if (document.querySelector('.mobile-filter__btn')) {
+    
+    const filterBtnOpen = document.querySelector('.mobile-filter__btn')
+    const filterBtnClose = document.querySelector('.mobile-filter__close')
+    const mobileFilter = document.querySelector('.mobile-filter__body')
+    const body = document.querySelector('body')
 
-if (filterBtnOpen) {
     filterBtnOpen.addEventListener('click', () => {
         if (mobileFilter.classList.contains('active')) {
             mobileFilter.classList.remove('active')
@@ -63,6 +93,25 @@ if (filterBtnOpen) {
             body.classList.add('_lock')
         }
     })
+}
+
+if (document.querySelectorAll('.js-tab-trigger')) {
+    var jsTriggers = document.querySelectorAll('.js-tab-trigger'),
+        jsContents = document.querySelectorAll('.js-tab-content');
+    jsTriggers.forEach(function(trigger) {
+        trigger.addEventListener('click', function() {
+            var id = this.getAttribute('data-tab'),
+            content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
+            activeTrigger = document.querySelector('.js-tab-trigger.active'),
+            activeContent = document.querySelector('.js-tab-content.active');
+        
+          activeTrigger.classList.remove('active'); // 1
+          trigger.classList.add('active'); // 2
+        
+          activeContent.classList.remove('active'); // 3
+          content.classList.add('active'); // 4
+        });
+    });
 }
 
 
